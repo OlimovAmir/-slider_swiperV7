59dc37a4b9c6971323ddb1ff3b7a04d5b0da74d9
OlimovAmir <olimov.amir@mail.ru>
2022-06-01 09:24:41 +0500

    2

--- app.js
+++ app.js
@@ -17,9 +17,30 @@ var swiper = new Swiper(".mySwiper", {
         let scriptPath = document.querySelector('#path');
         linkStyle.remove();
         scriptPath.remove();
+        
 
-        document.body.childNodes[1].childNodes[3].childNodes[1].style.cssText = `display: none`;
-      
+        document.body.childNodes[1].childNodes[1].style = `position: relative;`;
+
+        document.body.childNodes[1]
+          .childNodes[3].childNodes[1]
+            .style = `display: flex;
+                       justify-content: space-around;
+                       flex-wrap: wrap;`;
+
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[1].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[3].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[5].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[7].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[9].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[11].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[13].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[15].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[17].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[19].style = `display: inline;`;
+                       document.body.childNodes[1].childNodes[3].childNodes[1].childNodes[21].style = `display: inline;`;
+
+                      
+
 
     } else {
   
@@ -32,5 +53,7 @@ var swiper = new Swiper(".mySwiper", {
   
     }
   })
+
+  
   
  
